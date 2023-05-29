bb66a8f8b67ff99f87dc2754c822e93f88216f09
Victor <vityanesterovich2008@gmail.com>
2023-05-29 09:25:16 +0300

    add state

--- aliceAPI/Response.py
+++ aliceAPI/Response.py
@@ -4,7 +4,9 @@ from .ui import Button, BigImage
 
 class Response:
     def __init__(self, text: str='', tts: str='', end_session: bool=False,
-                 buttons: Iterable=[], card: BigImage | None=None):
+                 buttons: Iterable=[], card: BigImage | None=None,
+                 session_state: dict | None=None, user_state: dict | None=None,
+                 app_state: dict | None=None):
         
         self.text = text 
         self.tts = tts
@@ -12,6 +14,9 @@ class Response:
         self._version = '1.0'
         self.buttons = list(buttons)
         self.card = card
+        self.session_state = session_state
+        self.user_state = user_state
+        self.app_state = app_state
         
     def add_button(self, button: Button):
         ''' Добавляет одну кнопку '''
@@ -35,5 +40,11 @@ class Response:
             result['response']['buttons'] = [button.json() for button in self.buttons]
         if self.card is not None:
             result['response']['card'] = self.card.json()
+        if self.session_state is not None:
+            result['session_state'] = self.session_state
+        if self.user_state is not None:
+            result['user_state_update'] = self.user_state
+        if self.app_state is not None:
+            result['application_state'] = self.app_state
             
         return result
